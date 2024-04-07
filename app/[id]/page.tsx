5cd9bfe3dea182494b42d9194dc7c8e7fcca269b
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-04-07 17:08:43 +0800

    created env

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { CalendarFold, Eye, Lock } from 'lucide-react'
+import { AtSign, CalendarFold, Eye, Lock, MapPin } from 'lucide-react'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import Profile from '@/public/profile.avif'
@@ -21,38 +21,11 @@ const GET_USER_QUERY = gql`
   }
 `
 
-interface UserData {
-  firstName: string
-  lastName: string
-  birthdate: string
-  gender: string
-  address: string
-  email: string
-  contactNumber: string
-  password: string
-}
-
-interface UserCardProps {
-  data: UserData
-}
-
 const Page = ({ params }: { params: { id: string } }) => {
   const { data, loading, error } = useQuery(GET_USER_QUERY, {
     variables: { id: params.id },
   })
   const [showPassword, setShowPassword] = useState<boolean>(false)
-  // const data = {
-  //   firstName: 'John',
-  //   lastName: 'Doe',
-  //   birthdate: '1990-01-01',
-  //   gender: 'Male',
-  //   address: '123 Main St, Anytown, USA',
-  //   email: 'john.doe@example.com',
-  //   contactNumber: '123-456-7890',
-  //   password: 'test',
-  // }
-
-  console.log(data.user.firstName)
 
   const handleShowPassword = () => {
     setShowPassword(true)
@@ -93,42 +66,13 @@ const Page = ({ params }: { params: { id: string } }) => {
         </div>
 
         <div className='mt-4 flex items-center text-gray-700 '>
-          <svg
-            aria-label='location pin icon'
-            className='fillCurrent h-6 w-6'
-            viewBox='0 0 24 24'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              fillRule='evenodd'
-              clipRule='evenodd'
-              d='M16.2721 10.2721C16.2721 12.4813 14.4813 14.2721 12.2721 14.2721C10.063 14.2721 8.27214 12.4813 8.27214 10.2721C8.27214 8.063 10.063 6.27214 12.2721 6.27214C14.4813 6.27214 16.2721 8.063 16.2721 10.2721ZM14.2721 10.2721C14.2721 11.3767 13.3767 12.2721 12.2721 12.2721C11.1676 12.2721 10.2721 11.3767 10.2721 10.2721C10.2721 9.16757 11.1676 8.27214 12.2721 8.27214C13.3767 8.27214 14.2721 9.16757 14.2721 10.2721Z'
-            />
-            <path
-              fillRule='evenodd'
-              clipRule='evenodd'
-              d='M5.79417 16.5183C2.19424 13.0909 2.05438 7.3941 5.48178 3.79418C8.90918 0.194258 14.6059 0.0543983 18.2059 3.48179C21.8058 6.90919 21.9457 12.606 18.5183 16.2059L12.3124 22.7241L5.79417 16.5183ZM17.0698 14.8268L12.243 19.8965L7.17324 15.0698C4.3733 12.404 4.26452 7.9732 6.93028 5.17326C9.59603 2.37332 14.0268 2.26454 16.8268 4.93029C19.6267 7.59604 19.7355 12.0269 17.0698 14.8268Z'
-            />
-          </svg>
+          <MapPin />
 
           <h1 className='px-2 text-sm'>{data?.user.address}</h1>
         </div>
 
         <div className='mt-4 flex items-center text-gray-700'>
-          <svg
-            aria-label='email icon'
-            className='fillCurrent h-6 w-6'
-            viewBox='0 0 24 24'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              fillRule='evenodd'
-              clipRule='evenodd'
-              d='M3.00977 5.83789C3.00977 5.28561 3.45748 4.83789 4.00977 4.83789H20C20.5523 4.83789 21 5.28561 21 5.83789V17.1621C21 18.2667 20.1046 19.1621 19 19.1621H5C3.89543 19.1621 3 18.2667 3 17.1621V6.16211C3 6.11449 3.00333 6.06765 3.00977 6.0218V5.83789ZM5 8.06165V17.1621H19V8.06199L14.1215 12.9405C12.9499 14.1121 11.0504 14.1121 9.87885 12.9405L5 8.06165ZM6.57232 6.80554H17.428L12.7073 11.5263C12.3168 11.9168 11.6836 11.9168 11.2931 11.5263L6.57232 6.80554Z'
-            />
-          </svg>
+        <AtSign />
 
           <h1 className='px-2 text-sm'>{data?.user.email}</h1>
         </div>
