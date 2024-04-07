397c73205c8acb5824ff4a0b434f02539e2da689
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-04-07 16:37:21 +0800

    Connect to backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+'use client'
 import Form from '@/components/Form'
 import Image from 'next/image'
+import { ApolloProvider } from '@apollo/client'
+import apolloClient from '@/lib/apolloClient'
 
 export default function Home() {
   return (
     <main className=''>
+      {/* <ApolloProvider client={apolloClient}> */}
       <Form />
+      {/* </ApolloProvider> */}
     </main>
   )
 }
